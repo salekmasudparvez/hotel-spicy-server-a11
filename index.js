322632486549d86a908fccac335946b8bc25535c
Salek Masud Parvez <151811650+salekmasudparvez@users.noreply.github.com>
2024-05-12 16:39:10 +0600

    added mybook post and delete

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-app.use(cors());
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://solosphere.web.app",
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2gfcy7h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,7 +25,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -29,28 +36,87 @@ async function run() {
     const reviewCollection = client.db("hotelSpicyDB").collection("review");
     const roomsCollection = client.db("hotelSpicyDB").collection("rooms");
     const feturesCollection = client.db("hotelSpicyDB").collection("fetures");
+    const mybookingsCollection = client
+      .db("hotelSpicyDB")
+      .collection("mybooking");
 
-    app.get('/review', async(req,res)=>{
-        const cursor = reviewCollection.find();
-        const result = await cursor.toArray();
-        res.send(result)
+    app.get("/review", async (req, res) => {
+      const cursor = reviewCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/roomsCount", async (req, res) => {
+      const count = await roomsCollection.estimatedDocumentCount();
+      res.send({ count });
+    });
+    app.get("/rooms", async (req, res) => {
+      console.log(req.query); //hit server apli with data will show here result
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      const result = await roomsCollection
+        .find()
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+      res.send(result);
+    });
+    app.get("/rooms/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const singleRoom = await roomsCollection.findOne(filter);
+      res.send(singleRoom);
+    });
+    app.get("/fetures", async (req, res) => {
+      const cursor = feturesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.post("/mybooking", async (req, res) => {
+      const currentBooking = req.body;
+      const BookID = currentBooking.bookingID;
+      console.log(BookID);
+      const result = await mybookingsCollection.insertOne(currentBooking);
+      // update part
+      const updateDoc = {
+        $set: { Availability: false },
+      };
+      const myBookingQuery = { _id: new ObjectId(BookID) };
+      const updateBidCount = await roomsCollection.updateOne(
+        myBookingQuery,
+        updateDoc
+      );
+      console.log(updateBidCount);
 
-    })
-    app.get('/rooms', async(req,res)=>{
-        const cursor = roomsCollection.find();
-        const result = await cursor.toArray();
-        res.send(result)
+      res.send(result);
+    });
 
-    })
-    app.get('/fetures', async(req,res)=>{
-        const cursor = feturesCollection.find();
-        const result = await cursor.toArray();
-        res.send(result)
+    app.get("/mybooking", async (req, res) => {
+      const userEmail = req.query.email;
+      const queryRooms = { clientEmail: userEmail };
+      const result = await mybookingsCollection.find(queryRooms).toArray();
+      res.send(result);
+    });
+    app.delete("/mybooking/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = { bookingID: new ObjectId(id) };
 
-    })
+      const myBookingQuery = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: { Availability: true },
+      };
+
+      const updateBidCount = await roomsCollection.updateOne(
+        myBookingQuery,
+        updateDoc
+      );
+      const result = await mybookingsCollection.deleteOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -58,12 +124,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 app.get("/", (req, res) => {
-    res.send("amazon is busy shopping");
-  });
-  
-  app.listen(port, () => {
-    console.log(`amazon server is running on port: ${port}`);
-  });
+  res.send("amazon is busy shopping");
+});
+
+app.listen(port, () => {
+  console.log(`amazon server is running on port: ${port}`);
+});
